b71d1095c8cddbdc3c0d15c064da918149f1886a
nhelm <nhelm21@gmail.com>
2026-02-04 11:01:56 -0700

    Added homework directory with assignment one and updated README.md to include a description of the respositories structure.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-# BIOE591 Coursework Repository
+## github.com/nhelm/bioe591-git Repository
 
 This repository documents my initial attempts at using Git and GitHub.
-Github Edit via the website
 
+## Repository Structure
+
+- `README.md`
+The file you are currently reading. It describes the structure and contents of the `bioe591-git` repository on GitHub.
+
+- `homework/` 
+This subdirectory contains all assignments for the course, labeled as  
+  `ASSIGNMENT-NUMBER_Homework_Helmstetter`
---
+++ homework/01_Homework_Helmstetter.sh
@@ -0,0 +1,17 @@
+cd ~/Desktop/BIOE591_Genomics/HW1/
+curl -L -O https://raw.githubusercontent.com/elinck/genomics_eco_con/main/data/week_1.tar.gz # download a resource at a url
+tar -xzf week_1.tar.gz # this command unzips a "tarball"
+cd week_1 # enter the data directory
+
+mkdir fastq fasta metadata # Make three new directories
+
+mv *.gz fastq/ # move .gz files to fastq folder
+mv *.fasta fasta/ # move .fasta files to fasta folder
+mv *.csv metadata/ # move .csv files to metadata folder
+
+# Count the number of files in each directory
+ls -1 fastq/ | wc -l # list files in a directory, 1 per line; send output to wc function, count lines
+ls -1 fasta/ | wc -l
+ls -1 metadata/ | wc -l
+
+ls week_1 # check to see what's in this folder now
